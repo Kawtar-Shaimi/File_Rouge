f73189cee4af694a642b7bbbe61cc0cd1bb1654d
Kawtar <Kawtar.shaimi8@gmail.com>
2025-03-04 11:32:57 +0000

    Ajouter la relation entre Payment et Order dans le model Order

--- E-commerce/app/Models/Order.php
+++ E-commerce/app/Models/Order.php
@@ -15,4 +15,8 @@ class Order extends Model
     {
         return $this->belongsTo(User::class);
     }
+    public function payment()
+    {
+        return $this->hasOne(Payment::class);
+    }
 }
