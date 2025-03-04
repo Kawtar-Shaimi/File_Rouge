4faa1df0b0ab5f9f816bb9f8b3cf8f14ed84b324
Kawtar <Kawtar.shaimi8@gmail.com>
2025-03-04 11:42:23 +0000

    Ajouter AuthController pour l'authentification

---
+++ E-commerce/app/Http/Controllers/AuthController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Hash;
+use Illuminate\Container\Attributes\Auth;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function loginView()
+    {
+        return view('auth.login');
+    }
+
+    public function registerView()
+    {
+        return view('auth.register');
+    }
+
+    public function login(Request $request){
+        $validatedData = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        $remember = $request->has('remember');
+
+        if (Auth::attempt($validatedData, $remember)) {
+            return redirect()->route('home');
+        }
+
+        return back()->withErrors([
+            'email' => 'Invalid credentials.',
+        ]);
+    }
+
+    public function register(Request $request){
+        $validatedData = $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'role' => 'required|in:client,developer',
+            'password' => 'required|confirmed'
+        ]);
+
+        $validatedData['password'] = Hash::make($validatedData['password']);
+
+        $user = User::create($validatedData);
+
+        $remember = $request->has('remember');
+
+        Auth::login($user, $remember);
+
+        return redirect()->route('home');
+    }
+
+    public function logout(){
+        Auth::logout();
+
+        return redirect()->route('loginView');
+    }
+}
+
