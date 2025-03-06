5ad2b09a820d7e49a5ad0846d3461efb381ef218
Kawtar <Kawtar.shaimi8@gmail.com>
2025-03-06 10:14:29 +0000

    Modifier le header

--- E-commerce/resources/views/layouts/header.blade.php
+++ E-commerce/resources/views/layouts/header.blade.php
@@ -24,10 +24,18 @@
             </nav>
 
             <!-- User Menu -->
-            <div class="flex items-center space-x-4">
-                <!-- Avatar et menu dropdown -->
+            <div class="flex items-center space-x-2">
+                <!-- Shopping Cart Icon -->
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                    <path class="fill-white" d="M0 24C0 10.7 10.7 0 24 0L69.5 0c22 0 41.5 12.8 50.6 32l411 0c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3l-288.5 0 5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5L488 336c13.3 0 24 10.7 24 24s-10.7 24-24 24l-288.3 0c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5L24 48C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/>
+                </svg>
+                <a href="#" class="text-gray-200 hover:text-white transition-colors relative">
+                    <i class="fas fa-shopping-cart text-2xl"></i>
+                    <!-- Optionally add a badge for the number of items in the cart -->
+                    <span class="absolute top-0 right-0 bg-red-500 text-white text-xs rounded-full px-1">3</span>
+                </a>
                 <div class="relative">
-                    <button id="userMenuButton" class="flex items-center space-x-2">
+                    <button id="userMenuButton" class="flex items-center space-x-4">
                         <img src="https://ui-avatars.com/api/?name=User&background=6B7280&color=fff" alt="User Avatar" class="w-8 h-8 rounded-full">
                         <i class="fas fa-chevron-down"></i>
                     </button>
@@ -50,6 +58,7 @@
             <a href="#" class="block px-4 py-2 hover:bg-gray-700">Categories</a>
             <a href="#" class="block px-4 py-2 hover:bg-gray-700">About Us</a>
             <a href="#" class="block px-4 py-2 hover:bg-gray-700">Contact</a>
+            <button id="logoutButtonMobile" class="block w-full text-left px-4 py-2 hover:bg-gray-700">Logout</button>
         </nav>
     </header>
 
@@ -66,6 +75,14 @@
 
         // Gestion du logout (simple console.log)
         document.getElementById("logoutButton").addEventListener("click", function () {
+            console.log("Déconnexion...");
+        });
+
+        document.getElementById("logoutButtonDesktop").addEventListener("click", function () {
+            console.log("Déconnexion...");
+        });
+
+        document.getElementById("logoutButtonMobile").addEventListener("click", function () {
             console.log("Déconnexion...");
         });
 
